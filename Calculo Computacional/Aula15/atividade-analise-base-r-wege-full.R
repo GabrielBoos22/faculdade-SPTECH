3d50d9b3947d9eb6a21f1d94b1bc724556478bc0
GabrielBoos22 <gabriel.bduarte@sptech.school>
2023-11-16 10:17:49 -0300

    modelagem linear

--- Calculo Computacional/Aula15/atividade-analise-base-r-wege-full.R
+++ Calculo Computacional/Aula15/atividade-analise-base-r-wege-full.R
@@ -1,4 +1,11 @@
-plot(base.r.wege.fulll$Open, base.r.wege.full$Close)
+par(mfrow = c(3, 2))  
+
+plot(base.r.wege.full$Open, base.r.wege.full$Close, main = "Open vs Close", type = "l")
+plot(base.r.wege.full$Open, base.r.wege.full$High, main = "Open vs High", type = "l")
+plot(base.r.wege.full$Open, base.r.wege.full$Low, main = "Open vs Low", type = "l")
+plot(base.r.wege.full$High, base.r.wege.full$Low, main = "High vs Low", type = "l")
+plot(base.r.wege.full$High, base.r.wege.full$Close, main = "High vs Close", type = "l")
+plot(base.r.wege.full$High, base.r.wege.full$Open, main = "High vs Open", type = "l")
 
 modelo <- lm(base.r.wege.full$Open~base.r.wege.full$Close)
 summary(modelo)
@@ -6,4 +13,5 @@ summary(modelo)
 
 par(mfrow=c(2,2))
 plot(modelo, which = 1:4)
+
 
