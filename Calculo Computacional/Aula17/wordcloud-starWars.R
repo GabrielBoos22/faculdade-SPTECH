03646625ef15f010fe0c5c600d00a3a0acb6aad7
GabrielBoos22 <gabriel.bduarte@sptech.school>
2024-01-03 15:34:33 -0300

    WordCloud - Starwars

--- Calculo Computacional/Aula17/wordcloud-starWars.R
+++ Calculo Computacional/Aula17/wordcloud-starWars.R
@@ -447,8 +447,8 @@ library(tm)
 library(wordcloud)
 library(RColorBrewer)
 library(wordcloud2)
-starWars4 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeV_ptBR.txt", sep="")
-han <- subset(starWars4, starWars4$personagem == "HAN")
+starWars5 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeV_ptBR.txt", sep="")
+han <- subset(starWars5, starWars5$personagem == "HAN")
 docs <- Corpus(VectorSource(han$diálogo))
 docs <- tm_map(docs, tolower)
 docs <- tm_map(docs, removePunctuation)
@@ -459,7 +459,7 @@ docs <- tm_map(docs, removeWords, c("mim","deve","posso",
                                     "aqui","sim","bom","coisa","parte",
                                     "todas","per","etc","abaixo","outros",
                                     "pode","aqui","pode",
-                                    "ver","ser", "sei", "bem", "aí", "então", "vamos", "lá"))
+                                    "ver","ser", "sei", "bem", "aí", "então", "vamos", "lá", "vai", "acho", "tudo"))
 dtm <- TermDocumentMatrix(docs)
 matrix <- as.matrix(dtm)
 words <- sort(rowSums(matrix), decreasing = TRUE)
@@ -478,8 +478,8 @@ library(tm)
 library(wordcloud)
 library(RColorBrewer)
 library(wordcloud2)
-starWars4 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeV_ptBR.txt", sep="")
-luke <- subset(starWars4, starWars4$personagem == "LUKE")
+starWars5 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeV_ptBR.txt", sep="")
+luke <- subset(starWars5, starWars5$personagem == "LUKE")
 docs <- Corpus(VectorSource(luke$diálogo))
 docs <- tm_map(docs, tolower)
 docs <- tm_map(docs, removePunctuation)
@@ -509,8 +509,8 @@ library(tm)
 library(wordcloud)
 library(RColorBrewer)
 library(wordcloud2)
-starWars4 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeV_ptBR.txt", sep="")
-threepio <- subset(starWars4, starWars4$personagem == "THREEPIO")
+starWars5 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeV_ptBR.txt", sep="")
+threepio <- subset(starWars5, starWars5$personagem == "THREEPIO")
 docs <- Corpus(VectorSource(threepio$diálogo))
 docs <- tm_map(docs, tolower)
 docs <- tm_map(docs, removePunctuation)
@@ -535,14 +535,203 @@ wordcloud(words=df$word, freq=df$freq, min.freq=1,
 wordcloud2(data=df, size=0.7, color="random-light", backgroundColor="black",
            minRotation=-pi/6, maxRotation=-pi/6, rotateRatio=0)
 
-#BEN
+#LANDO
 library(tm)
 library(wordcloud)
 library(RColorBrewer)
 library(wordcloud2)
-starWars4 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeV_ptBR.txt", sep="")
-ben <- subset(starWars4, starWars4$personagem == "BEN")
+starWars5 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeV_ptBR.txt", sep="")
+ben <- subset(starWars5, starWars5$personagem == "LANDO")
 docs <- Corpus(VectorSource(ben$diálogo))
+docs <- tm_map(docs, tolower)
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, removeWords, stopwords("pt"))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, removeWords, c("mim","deve","posso",
+                                    "aqui","sim","bom","coisa","parte",
+                                    "todas","per","etc","abaixo","outros",
+                                    "pode","aqui","pode",
+                                    "ver","ser", "sei", "tudo"))
+dtm <- TermDocumentMatrix(docs)
+matrix <- as.matrix(dtm)
+words <- sort(rowSums(matrix), decreasing = TRUE)
+df <- data.frame(word=names(words), freq=words)
+
+
+wordcloud(words=df$word, freq=df$freq, min.freq=1,
+          max.words=50, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"), scale=c(2,1))
+
+wordcloud2(data=df, size=0.7, color="random-light", backgroundColor="black",
+           minRotation=-pi/6, maxRotation=-pi/6, rotateRatio=0)
+#LEIA
+
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+library(wordcloud2)
+starWars5 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeV_ptBR.txt", sep="")
+leia <- subset(starWars5, starWars5$personagem == "LEIA")
+docs <- Corpus(VectorSource(leia$diálogo))
+docs <- tm_map(docs, tolower)
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, removeWords, stopwords("pt"))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, removeWords, c("mim","deve","posso",
+                                    "aqui","sim","bom","coisa","parte",
+                                    "todas","per","etc","abaixo","outros",
+                                    "pode","aqui","pode",
+                                    "ver","ser", "sei", "aí", "lá", "quê", "então","achei"))
+dtm <- TermDocumentMatrix(docs)
+matrix <- as.matrix(dtm)
+words <- sort(rowSums(matrix), decreasing = TRUE)
+df <- data.frame(word=names(words), freq=words)
+
+
+wordcloud(words=df$word, freq=df$freq, min.freq=1,
+          max.words=50, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"), scale=c(2,1))
+
+wordcloud2(data=df, size=0.7, color="random-light", backgroundColor="black",
+           minRotation=-pi/6, maxRotation=-pi/6, rotateRatio=0)#
+
+#FILME 6 ------------------------------------------------------------------------------------------------------
+# Word Cloud personagens
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+library(wordcloud2)
+starWars6 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeVI_ptBR.txt", sep="")
+personagens <- starWars6$personagem
+docs <- Corpus(VectorSource(personagens))
+docs <- tm_map(docs, tolower)
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, removeWords, stopwords("pt"))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, removeWords, c("mim","deve","posso",
+                                    "aqui","sim","bom","coisa","parte",
+                                    "todas","per","etc","abaixo","outros",
+                                    "pode","aqui","cetax",
+                                    "ver","ser"))
+dtm <- TermDocumentMatrix(docs)
+matrix <- as.matrix(dtm)
+words <- sort(rowSums(matrix), decreasing = TRUE)
+df <- data.frame(word=names(words), freq=words)
+
+wordcloud(words=df$word, freq=df$freq, min.freq=1,
+          max.words=50, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"), scale=c(2,1))
+
+wordcloud2(data=df, size=0.7, color="random-light", backgroundColor="black",
+           minRotation=-pi/6, maxRotation=-pi/6, rotateRatio=0)
+
+#Word cloud diálogo
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+library(wordcloud2)
+starWars6 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeVI_ptBR.txt", sep="")
+dialogo <- starWars6$diálogo
+docs <- Corpus(VectorSource(dialogo))
+docs <- tm_map(docs, tolower)
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, removeWords, stopwords("pt"))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, removeWords, c("mim","deve","posso",
+                                    "aqui","sim","bom","coisa","parte",
+                                    "todas","per","etc","abaixo","outros",
+                                    "pode","aqui","pode",
+                                    "ver","ser", "sei", "bem", "pode", "tão", "lá", "ter", "vai", "vou", "acho","vamos","sabe","nada","vai","algo","vou", "tudo","então"))
+dtm <- TermDocumentMatrix(docs)
+matrix <- as.matrix(dtm)
+words <- sort(rowSums(matrix), decreasing = TRUE)
+df <- data.frame(word=names(words), freq=words)
+
+
+
+wordcloud(words=df$word, freq=df$freq, min.freq=1,
+          max.words=50, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"), scale=c(2,1))
+
+wordcloud2(data=df, size=0.7, color="random-light", backgroundColor="black",
+           minRotation=-pi/6, maxRotation=-pi/6, rotateRatio=0)
+
+#subset personagens luke, han e threepio
+
+#HAN
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+library(wordcloud2)
+starWars6 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeVI_ptBR.txt", sep="")
+han <- subset(starWars6, starWars5$personagem == "HAN")
+docs <- Corpus(VectorSource(han$diálogo))
+docs <- tm_map(docs, tolower)
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, removeWords, stopwords("pt"))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, removeWords, c("mim","deve","posso",
+                                    "aqui","sim","bom","coisa","parte",
+                                    "todas","per","etc","abaixo","outros",
+                                    "pode","aqui","pode",
+                                    "ver","ser", "sei", "bem", "aí", "então", "vamos", "lá", "vai", "acho", "tudo"))
+dtm <- TermDocumentMatrix(docs)
+matrix <- as.matrix(dtm)
+words <- sort(rowSums(matrix), decreasing = TRUE)
+df <- data.frame(word=names(words), freq=words)
+
+
+wordcloud(words=df$word, freq=df$freq, min.freq=1,
+          max.words=50, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"), scale=c(2,1))
+
+wordcloud2(data=df, size=0.7, color="random-light", backgroundColor="black",
+           minRotation=-pi/6, maxRotation=-pi/6, rotateRatio=0)
+
+#luke
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+library(wordcloud2)
+starWars6 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeVI_ptBR.txt", sep="")
+luke <- subset(starWars6, starWars6$personagem == "LUKE")
+docs <- Corpus(VectorSource(luke$diálogo))
+docs <- tm_map(docs, tolower)
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, removeWords, stopwords("pt"))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, removeWords, c("mim","deve","posso",
+                                    "aqui","sim","bom","coisa","parte",
+                                    "todas","per","etc","abaixo","outros",
+                                    "pode","aqui","pode",
+                                    "ver","ser", "sei", "bem", "aí", "então"))
+dtm <- TermDocumentMatrix(docs)
+matrix <- as.matrix(dtm)
+words <- sort(rowSums(matrix), decreasing = TRUE)
+df <- data.frame(word=names(words), freq=words)
+
+
+wordcloud(words=df$word, freq=df$freq, min.freq=1,
+          max.words=50, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"), scale=c(2,1))
+
+wordcloud2(data=df, size=0.7, color="random-light", backgroundColor="black",
+           minRotation=-pi/6, maxRotation=-pi/6, rotateRatio=0)
+
+#threepio
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+library(wordcloud2)
+starWars6 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeVI_ptBR.txt", sep="")
+threepio <- subset(starWars6, starWars6$personagem == "THREEPIO")
+docs <- Corpus(VectorSource(threepio$diálogo))
 docs <- tm_map(docs, tolower)
 docs <- tm_map(docs, removePunctuation)
 docs <- tm_map(docs, removeWords, stopwords("pt"))
@@ -565,14 +754,45 @@ wordcloud(words=df$word, freq=df$freq, min.freq=1,
 
 wordcloud2(data=df, size=0.7, color="random-light", backgroundColor="black",
            minRotation=-pi/6, maxRotation=-pi/6, rotateRatio=0)
+
+#VADER
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+library(wordcloud2)
+starWars6 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeVI_ptBR.txt", sep="")
+vader <- subset(starWars5, starWars5$personagem == "VADER")
+docs <- Corpus(VectorSource(vader$diálogo))
+docs <- tm_map(docs, tolower)
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, removeWords, stopwords("pt"))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, removeWords, c("mim","deve","posso",
+                                    "aqui","sim","bom","coisa","parte",
+                                    "todas","per","etc","abaixo","outros",
+                                    "pode","aqui","pode",
+                                    "ver","ser", "sei", "tudo"))
+dtm <- TermDocumentMatrix(docs)
+matrix <- as.matrix(dtm)
+words <- sort(rowSums(matrix), decreasing = TRUE)
+df <- data.frame(word=names(words), freq=words)
+
+
+wordcloud(words=df$word, freq=df$freq, min.freq=1,
+          max.words=50, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"), scale=c(2,1))
+
+wordcloud2(data=df, size=0.7, color="random-light", backgroundColor="black",
+           minRotation=-pi/6, maxRotation=-pi/6, rotateRatio=0)
 #LEIA
 
 library(tm)
 library(wordcloud)
 library(RColorBrewer)
 library(wordcloud2)
-starWars4 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeV_ptBR.txt", sep="")
-leia <- subset(starWars4, starWars4$personagem == "LEIA")
+starWars6 <- read.csv("C:/Users/Vayon24/Desktop/sptech/2-sem-SPTech/Calculo Computacional/Aula17/SW_EpisodeVI_ptBR.txt", sep="")
+leia <- subset(starWars6, starWars6$personagem == "LEIA")
 docs <- Corpus(VectorSource(leia$diálogo))
 docs <- tm_map(docs, tolower)
 docs <- tm_map(docs, removePunctuation)
@@ -583,7 +803,7 @@ docs <- tm_map(docs, removeWords, c("mim","deve","posso",
                                     "aqui","sim","bom","coisa","parte",
                                     "todas","per","etc","abaixo","outros",
                                     "pode","aqui","pode",
-                                    "ver","ser", "sei", "aí", "lá"))
+                                    "ver","ser", "sei", "aí", "lá", "quê", "então","achei", "vamos", "vai"))
 dtm <- TermDocumentMatrix(docs)
 matrix <- as.matrix(dtm)
 words <- sort(rowSums(matrix), decreasing = TRUE)
